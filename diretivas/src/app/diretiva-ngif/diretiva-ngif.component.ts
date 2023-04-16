0091ba177fb9a07ad56a592b77489d5897488f82
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-16 19:37:52 -0300

    Aula 25: Diretivas: ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./diretiva-ngif.component.css']
 })
 export class DiretivaNgifComponent {
+  cursos :string[] = ['Angular 2'];
 
+  mostrarCursos :boolean = false;
+
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
