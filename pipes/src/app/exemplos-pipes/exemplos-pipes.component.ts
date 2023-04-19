d681509a8bb8c9670e7d698508e8c42a0c2b04ce
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-18 22:45:32 -0300

    Aula 43: Pipes (usando pipes, parâmetros e pipes aninhados)

---
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent {
+
+  livro: any = {
+    titulo: "Título Livro",
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glgjpRP'
+  };
+}
