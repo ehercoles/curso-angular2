cad9a9edc616a66cf97b3310270fcda97f805a53
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-20 18:50:06 -0300

    Aula 61: Rotas Filhas: desenvolvendo as telas

--- rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
+++ rotas/src/app/alunos/aluno-detalhe/aluno-detalhe.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlunosService } from '../alunos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-aluno-detalhe',
   templateUrl: './aluno-detalhe.component.html',
   styleUrls: ['./aluno-detalhe.component.css']
 })
-export class AlunoDetalheComponent {
 
+export class AlunoDetalheComponent implements OnInit, OnDestroy {
+  aluno: any;
+  inscricao!: Subscription;
+
+  constructor(
+    private rout: ActivatedRoute,
+    private router: Router,
+    private alunosService: AlunosService
+  ) { }
+
+  ngOnInit() {
+    this.inscricao = this.rout.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+      }
+    );
+  }
+
+  editarContato() {
+    this.router.navigate(['/alunos', this.aluno.id, 'editar']);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
 }
--- rotas/src/app/alunos/aluno-form/aluno-form.component.ts
+++ rotas/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlunosService } from '../alunos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent {
 
+export class AlunoFormComponent implements OnInit, OnDestroy {
+  aluno: any;
+  inscricao!: Subscription;
+  private formMudou: boolean = false;
+
+  constructor(
+    private rout: ActivatedRoute,
+    private alunosService: AlunosService
+  ) { }
+
+  ngOnInit() {
+    this.inscricao = this.rout.params.subscribe(
+      (params: any) => {
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+
+        if (this.aluno === null) {
+          this.aluno = {};
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  onInput() {
+    this.formMudou = true;
+    console.log('mudou');
+  }
+
+  podeMudarRota() {
+    if (this.formMudou) {
+      confirm('Tem certeza que deseja sair dessa página?');
+    }
+
+    return true;
+  }
+
+  podeDesativar(){
+    return this.podeMudarRota();
+  }
 }
