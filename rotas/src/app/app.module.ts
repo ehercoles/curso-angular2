2c30c38640a869729663c864855f9ca983b23424
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-20 14:32:58 -0300

    Aula 57: Rotas: Criando um módulo de rotas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -22,8 +21,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    routing
+    AppRoutingModule
+    //routing
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,10 +1,10 @@
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
