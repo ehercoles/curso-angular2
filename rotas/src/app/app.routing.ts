5b76da253774944a168457dc713b0bb9323e1aaa
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-20 11:41:06 -0300

    Aula 53: Rotas: Definindo e extraindo parâmetros de roteamento | materialize fix

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'cursos', component: CursosComponent }
+    { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent }
 ];
 
 export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
