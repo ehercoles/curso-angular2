92a623c6f67ab92c00b92afc7f98322afa0b6f05
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-21 09:47:14 -0300

    Aula 63: Rotas: Tela de Login e como não mostrar o Menu (NavBar)

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './login/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,10 @@ import { LoginComponent } from './login/login.component';
     BrowserModule,
     //CursosModule,
     //AlunosModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ rotas/src/app/login/auth.service.ts
@@ -0,0 +1,24 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Usuario } from './usuario';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private usuarioAutenticado: boolean = false;
+  mostraMenu = new EventEmitter<boolean>();
+
+  constructor(private router: Router) { }
+
+  fazerLogin(usuario: Usuario) {
+    if (usuario.nome === 'usuario@email.com'
+      && usuario.senha === '123456') {
+
+      this.usuarioAutenticado = true;
+      this.mostraMenu.emit(true);
+      this.router.navigate(['/']);
+    }
+  }
+}
