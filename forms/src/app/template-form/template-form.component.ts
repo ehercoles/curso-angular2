6ffc4eacf1289ab648d04d2dffc948df9f1c74e8
Erasmus Ercoles <ehercoles@gmail.com>
2023-04-26 10:44:01 -0300

    Aula 85: Forms (template driven) Pesquisando endereço automaticamente com CEP

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -12,19 +13,48 @@ export class TemplateFormComponent {
     email: null
   }
 
+  constructor(private http: HttpClient) { }
+
   onSubmit(form: any) {
     console.log(form);
     //console.log(this.usuario);
   }
 
   verificaValidTouched(campo: any) {
-    return !campo.valid && campo.touched
+    return !campo.valid && campo.touched;
   }
 
   aplicaCssErro(campo: any) {
     return {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
+    };
+  }
+
+  consultaCep(campoCep: any, form: any) {
+    var cep = campoCep.value;
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != '') {
+      var validaCep = /^[0-9]{8}$/;
+      
+      if (validaCep.test(cep)) {
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .subscribe(dados => this.populaDadosForm(dados, form));
+      }
     }
+  }
+
+  populaDadosForm(dados :any, formulario: any) {
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
   }
 }
