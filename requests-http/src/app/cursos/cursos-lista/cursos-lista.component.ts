e51eeb4db68833d04070f73d6006e1672c736f85
Erasmus Ercoles <ehercoles@gmail.com>
2023-05-01 09:00:05 -0300

    Aula 120: Http GET: listar registros

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.scss']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true // Espaço entre botões (&nbsp;)
 })
-export class CursosListaComponent {
+export class CursosListaComponent implements OnInit {
 
+  cursos!: Curso[];
+
+  constructor(private service: CursosService) { }
+
+  ngOnInit() {
+    this.service.list().subscribe(dados => this.cursos = dados);
+  }
 }
