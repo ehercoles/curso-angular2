495c5229b762e69131ef9cb8d390721894aae74c
Erasmus Ercoles <ehercoles@gmail.com>
2023-05-01 17:21:26 -0300

    Aula 123 | Criação dos arquivos

--- requests-http/src/app/app-routing.module.ts
+++ requests-http/src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
   {
     path: 'cursos',
     loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule)
+  },
+  {
+    path: 'rxjs-poc',
+    loadChildren: () => import('./unsubscribe-rxjs/unsubscribe-rxjs.module')
+      .then(m => m.UnsubscribeRxjsModule)
   }
 ];
 
@@ -15,4 +20,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
