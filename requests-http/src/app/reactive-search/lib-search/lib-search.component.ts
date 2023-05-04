0a8f685a069b038170f73d203a60782b820eaaa3
Erasmus Ercoles <ehercoles@gmail.com>
2023-05-03 22:43:46 -0300

    Aula 144: Http: Pesquisa/Busca com Programação Reativa

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,31 +9,43 @@ import { Observable, map, tap } from 'rxjs';
   styleUrls: ['./lib-search.component.scss']
 })
 
-export class LibSearchComponent {
+export class LibSearchComponent implements OnInit {
 
   queryControl = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
   results$!: Observable<any>;
   total!: number;
+  readonly FIELDS = 'name,description,version,homepage';
 
   constructor(private http: HttpClient) { }
 
+  ngOnInit() {
+    this.results$ = this.queryControl.valueChanges
+      .pipe(
+        map(value => value.trim()),
+        filter(value => value.length > 1),
+        debounceTime(200),
+        distinctUntilChanged(),
+        //tap(value => console.log(value)),
+        switchMap(value => this.http.get(this.SEARCH_URL, {
+          params: {
+            search: value,
+            fields: this.FIELDS
+          }
+        })),
+        tap((res: any) => this.total = res.total),
+        map((res: any) => res.results));
+      //.subscribe(); // switchMap dispensa inscrição
+  }
+  
   onSearch() {
-    const fields = 'name,description,version,homepage';
     let value = this.queryControl.value;
 
     if (value && (value = value.trim()) !== '') {
-
-      /*
-      const params = {
-        search: value,
-        fields: fields
-      };
-      */
-
+      //const params = { search: value, fields: fields };
       let params = new HttpParams();
       params = params.set('search', value);
-      params = params.set('fields', fields);
+      params = params.set('fields', this.FIELDS);
 
       this.results$ = this.http
         .get(this.SEARCH_URL, { params })
